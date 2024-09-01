b29cff96fe1bfd7ca00892073074191b71403320
Aviv <rubysrubys@gmail.com>
2024-09-01 17:07:16 +0200

    Stuff

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,21 +1,23 @@
-import { differenceInMilliseconds, formatDistanceToNow } from 'date-fns'
+import { differenceInMilliseconds, formatDistanceToNowStrict } from 'date-fns'
 import {
   OutputSchema as RepoEvent,
   isCommit,
 } from './lexicon/types/com/atproto/sync/subscribeRepos'
-import { FirehoseSubscriptionBase } from './util/subscription'
+import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
   i: number = 0
 
   async handleEvent(evt: RepoEvent) {
     if (!isCommit(evt)) return
+    const ops = await getOpsByType(evt)
 
     if (this.i++ % 10_000 === 0) {
       const eventDate = new Date(evt.time)
       const diffMs = differenceInMilliseconds(new Date(), eventDate)
-      const diffWords = formatDistanceToNow(eventDate, {
+      const diffWords = formatDistanceToNowStrict(eventDate, {
         addSuffix: true,
+        unit: 'second',
       })
 
       console.log(
